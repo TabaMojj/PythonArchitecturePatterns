cf7926d8c65358ccc40bf686db9ace44a685536a
Mojtaba Mir Yaghoobzdeh <mirmojtaba21@gmail.com>
2023-04-29 18:27:57 +0330

    Add chapter 5 codes

--- service_layer/services.py
+++ service_layer/services.py
@@ -1,3 +1,6 @@
+from datetime import date
+from typing import Optional
+
 from domain import model
 from domain.model import OrderLine
 from adapters.repository import AbstractRepository
@@ -11,10 +14,16 @@ def is_valid_sku(sku, batches):
     return sku in {b.sku for b in batches}
 
 
-def allocate(line: OrderLine, repo: AbstractRepository, session) -> str:
+def allocate(orderid: str, sku: str, qty: int, repo: AbstractRepository, session) -> str:
     batches = repo.list()
+    line = model.OrderLine(orderid, sku, qty)
     if not is_valid_sku(line.sku, batches):
         raise InvalidSku(f'Invalid sku {line.sku}')
     batchref = model.allocate(line, batches)
     session.commit()
     return batchref
+
+
+def add_batch(ref: str, sku: str, qty: int, eta: Optional[date], repo: AbstractRepository, session):
+    repo.add(model.Batch(ref, sku, qty, eta))
+    session.commit()
